1fe582276c77fb3690cbc0b82039850170371e38
2021331096 <2021331096@student.sust.edu>
2023-08-10 19:58:42 +0600

    a

---
+++ script.js
@@ -0,0 +1,37 @@
+const myLink = "folklore.json";
+let song = [];
+fetch(myLink)
+  .then((a) => a.json())
+  .then((a) => {
+    song.push(...a);
+  });
+function matchin(tex, sss) {
+  const regex = RegExp(tex, "gi");
+  return sss.filter((a) => a.lyrics.match(regex) || a.name.match(regex));
+}
+function dropdown(a) {
+  const con = document.querySelector(`.${a}`);
+  con.style.display = con.style.display !== "block" ? "block" : "none";
+}
+function display() {
+  const a = matchin(this.value, song);
+  const regex = RegExp(this.value, "gi");
+  const html = a.map((a) => {
+    return `<li class="list">
+    <span class="name" onclick = "dropdown('${a.name}')">${a.name
+      .split("_")
+      .join(" ")}</span>
+    <span class="lyr ${a.name}">${a.lyrics.replace(
+      regex,
+      `<b class="alu">${this.value}</b>`
+    )}</span>
+    <span class-"link"><a href="${
+      a.youtube[0]
+    }" target="_blank">listen on youtube</a></span>
+    </li>`;
+  });
+  results.innerHTML = html.join("");
+}
+const inputs = document.querySelector(".sea");
+const results = document.querySelector(".result");
+inputs.addEventListener("keyup", display);
