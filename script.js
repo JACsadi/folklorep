7a7d135533f55ee7c8bc2731c3493f37588a1e28
2021331096 <2021331096@student.sust.edu>
2023-08-12 00:45:00 +0600

    n

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ let i = 1;
 function bim() {
   document.body.style.backgroundImage = `url(${bimage[i % 4]})`;
   console.log(`url(${bimage[i % 4]})`);
-  if (document.querySelector(".lyr") != null) {
+  if (document.querySelector(".lyr")) {
     if (i % 4 == 2)
       document.querySelector(".lyr").style.color = `rgba(148, 134, 174, 0.9)`;
     else document.querySelector(".lyr").style.color = `black`;
